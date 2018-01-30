0a7193e1d14296851ba9304112b42b91b9e89ed4
Sebastian Pauka <spauka@gmail.com>
2018-01-30 13:38:00 +1100

    Field sweeps, no switching during sweep

--- measurements.py
+++ measurements.py
@@ -6,9 +6,11 @@ Created on Mon Jan 22 18:56:19 2018
 """
 
 from time import sleep, clock
+from collections import Iterable
 import requests
 import qcodes as qc
 from qcodes import Parameter, MultiParameter
+import os, re
 
 class TimeParam(Parameter):
     def __init__(self, waittime):
@@ -17,14 +19,12 @@ class TimeParam(Parameter):
         super().__init__("time",
              label="Time",
              unit="s",
-             snapshot_get=False,
-             get_cmd=self._get,
-             set_cmd=self._set)
+             snapshot_get=False)
     
-    def _get(self):
+    def get_raw(self):
         return clock() - self._start
     
-    def _set(self, val):
+    def set_raw(self, val):
         sleep(self._waittime)
         
 class FridgeTemps(MultiParameter):
@@ -46,7 +46,7 @@ class FridgeTemps(MultiParameter):
                 units=tuple("K" for _ in params),
                 snapshot_get=False)
         
-    def get(self):
+    def get_raw(self):
         temps = requests.get(self.url)
         if temps.status_code != 200:
             raise RuntimeError("Unable to query fridge")
@@ -84,7 +84,7 @@ class LockinResistances(MultiParameter):
             units=tuple(param.unit for param in params),
             snapshot_get=False)
     
-    def get(self):
+    def get_raw(self):
         return [param.get() for param in self.params]
     
 class LockinSwitchResistances(MultiParameter):
@@ -99,7 +99,7 @@ class LockinSwitchResistances(MultiParameter):
             units=tuple(param.units[0] for _ in pairs),
             snapshot_get=False)
     
-    def get(self):
+    def get_raw(self):
         items = []
         for pair in self.pairs:
             self.md.select(pair)
@@ -107,15 +107,70 @@ class LockinSwitchResistances(MultiParameter):
             items.append(self.param.get())
         self.md.clear()
         return tuple(items)
-        
 
+class LockinsComb(MultiParameter):
+    def __init__(self, params):
+        self.params = params
+        self.param_name = params[0].name
+        super().__init__(name="Lockins_{}".format(self.param_name),
+              names=tuple("{}".format(param.full_name) for param in self.params),
+              shapes=tuple(() for _ in self.params),
+              units=tuple(param.unit for param in self.params),
+              snapshot_get=False)
+    
+    def get_raw(self):
+        return [param.get() for param in self.params]
+
+class MBLockins(MultiParameter):
+    def __init__(self, md, pairs, params, waittime):
+        self.pairs = pairs
+        self.md = md
+        self.waittime = waittime
+        if not isinstance(params, Iterable):
+            params = (params,)
+        self.params = params
+        
+        shapes = []
+        names = []
+        units = []
+        for pair in pairs:
+            for param in params:
+                names.append("{}_{}".format(pair, param.full_name))
+                if isinstance(param, MultiParameter):
+                    shapes.append((len(param.shapes),))
+                    units.append(param.units)
+                else:
+                    shapes.append(())
+                    units.append(param.unit)
+        shapes = tuple(shapes)
+        names = tuple(names)
+        units = tuple(units)
+        
+        super().__init__(name="MB_Switched",
+            names=names,
+            shapes=shapes,
+            units=units,
+            snapshot_get=False)
+    
+    def get_raw(self):
+        items = []
+        i = 0
+        for pair in self.pairs:
+            self.md.select(pair)
+            sleep(self.waittime)
+            for param in self.params:
+                items.append(param.get())
+                i += 1
+        self.md.clear()
+        return tuple(items)
+    
 def gen_resistances_param(lockins):
     params = [LockinResistance(lockin) for lockin in lockins]
     
     return LockinResistances(params)
 
 def meas_all(md, pairs, param):
-    lsr = LockinSwitchResistances(md, pairs, param, 10)
+    lsr = MBLockins(md, pairs, param, 10)
     m = qc.Measure(lsr)
     data = m.get_data_set()
     m.run()
@@ -139,3 +194,21 @@ def cooldown_loop(t, ft, resist, loops):
     loop.with_bg_task(p)
     loop.run()
     return data
+
+def field_sweep(ami, voltages, start, stop, points):
+    loop = qc.Loop(ami.field.sweep(start, stop, num=points))
+    if isinstance(voltages, Iterable):
+        loop = loop.each(*voltages)
+    else:
+        loop = loop.each(voltages)
+    data = loop.get_data_set()
+    loop.run()
+    return data
+
+def find_data(date, num):
+    date = str(date)
+    data_dir = os.path.join("data", date)
+    files = os.listdir(data_dir)
+    for file in files:
+        if re.match("#{:03}".format(num), file):
+            return os.path.join(data_dir, file)
--- startup.py
+++ startup.py
@@ -2,6 +2,7 @@
 
 import qcodes as qc
 from qcodes.instrument_drivers.stanford_research import SR860
+from qcodes.instrument_drivers.american_magnetics import AMI430
 from MulberryDriver import MulberryDriver
 
 # Close any instruments that may already be open
@@ -25,15 +26,33 @@ for i in range(4):
 md = MulberryDriver("md", "ASRL6::INSTR")
 station.add_component(md)
 
+# Connect to magnet backend
+ami = AMI430.AMI430("ami", "192.168.0.20", 7180)
+station.add_component(ami)
+
 from measurements import *
 
+# Lockin resistance measurements
 resistances = gen_resistances_param(lockins[:3])
 lsr = LockinSwitchResistances(md, ("B", "C", "D", "E"), resistances, 10)
 ft = FridgeTemps("BlueFors_LD", 
      "https://qphys1114.research.ext.sydney.edu.au/therm_flask/BlueFors_LD/data/?current")
 t = TimeParam(60)
 
-
+# Lockin voltage measurements
+voltages_X = [lockin.X for lockin in lockins]
+voltages_Y = [lockin.Y for lockin in lockins]
+voltages_R = [lockin.R for lockin in lockins]
+#resistances = [LockinResistance(lockins[1])]
+voltages = [item for sublist in zip(voltages_X, voltages_Y, voltages_R) for item in sublist]
+mb_voltages = MBLockins(md, ("B", "C", "D", "E"), voltages, 3)
 
 #h5fmt = hdf5_format.HDF5Format()
 #qc.DataSet.default_formatter = h5fmt
+
+def do_field_sweeps(md, pairs, params):
+    for pair in pairs:
+        md.select(pair)
+        delay = TimeParam(1)
+        field_sweep(ami, params + [delay], ami.field(), -ami.field(), 1000)
+    md.clear()
